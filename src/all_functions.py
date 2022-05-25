8e57020e6cb60587f42091b25a9ba22affa63dea
finlay <finlay.gray@sainsburys.co.uk>
2022-05-25 13:10:58 +0100

    auto update

--- src/all_functions.py
+++ src/all_functions.py
@@ -242,12 +242,95 @@ def add_courier(couriers):
     return couriers
 
 
+def update_courier(couriers):
+    clear_screen()
+    if couriers != []:
+        couriers_list_index(couriers)
+        try:
+            courier_index = int(
+                input('Enter index of courier you would like to update\n> ').strip())
+            courier_old_name = couriers[courier_index]
+            courier_new_name = input(
+                '\nEnter new name of courier\n> ').lower().strip()
+            if courier_new_name in couriers:
+                print('\nThis name already exits in your list\n')
+            else:
+                couriers[courier_index] = courier_new_name
+                print(
+                    f'\n{courier_old_name} has been replaced with {courier_new_name}\n')
+            input('Press enter to continue')
+        except:
+            print('\nPLease enter valid input\n')
+            input('Press enter to continue')
+    else:
+        print('\nYou do not have any couriers to update\n')
+        input('Press enter to continue')
 
+    return couriers
 
+def delete_courier(couriers,orders):
+    clear_screen()
+    if couriers != []:
+        couriers_list_index(couriers)
+        try:
+            courier_index = int(
+                input('Enter index of courier you would like to remove\n> ').strip())
+            clear_screen()
+            courier_order = []
+            for i in range(len(orders)):
+                if orders[i]["courier index"] == courier_index:
+                    courier_order.append(i)
+                elif orders[i]["courier index"] > courier_index:
+                    orders[i]["courier index"] -= 1
 
+            while True:
+                choice = input(f'''This courier is currently active in {len(courier_order)} orders
 
+Therefore please pick what you would like to do:
 
+Enter 1 to delete the courier and delete all orders associated with it
 
+Enter 2 to delete the courier and change the courier of all the associated couriers
+
+Enter 3 to not delete the courier
+> ''')
+
+                if choice == '1':
+                    clear_screen()
+                    courier_name = couriers[courier_index]
+                    couriers.pop(courier_index)
+                    print(
+                        f'\n{courier_name} has been removed from your couriers\n')
+                    for i in courier_order[::-1]:
+                        orders.pop(i)
+                    break
+                elif choice == '2':
+                    clear_screen()
+                    courier_name = couriers[courier_index]
+                    couriers.pop(courier_index)
+                    print(
+                        f'\n{courier_name} has been removed from your couriers\n')
+                    for i in courier_order:
+                        print(f'order to change: {orders[i]}')
+                        couriers_list_index(couriers)
+                        cour = int(
+                            input('\nWhich courier index would you like to switch to\n> '))
+                        orders[i]["courier index"] = cour
+                        clear_screen()
+                    break
+
+                elif choice == '3':
+                    for i in range(len(orders)):
+                        if orders[i]["courier index"] >= courier_index:
+                            orders[i]["courier index"] += 1
+                    break
+
+        except:
+            print('\nYou have not entered a valid index\n')
+    else:
+        print('\nYou do not have any couriers to delete\n')
+    input('Press enter to continue')
+    return couriers,orders
 
 
 # function that handles the product menu
@@ -287,91 +370,10 @@ def courier_menu_func(couriers, orders, courier_menu_text):
         couriers = add_courier(couriers)
     # allows user to update the name of a courier in the list
     elif option == '3':
-        clear_screen()
-        if couriers != []:
-            couriers_list_index(couriers)
-            try:
-                courier_index = int(
-                    input('Enter index of courier you would like to update\n> ').strip())
-                courier_old_name = couriers[courier_index]
-                courier_new_name = input(
-                    '\nEnter new name of courier\n> ').lower().strip()
-                if courier_new_name in couriers:
-                    print('\nThis name already exits in your list\n')
-                else:
-                    couriers[courier_index] = courier_new_name
-                    print(
-                        f'\n{courier_old_name} has been replaced with {courier_new_name}\n')
-                input('Press enter to continue')
-            except:
-                print('\nPLease enter valid input\n')
-                input('Press enter to continue')
-        else:
-            print('\nYou do not have any couriers to update\n')
-            input('Press enter to continue')
+        couriers = update_courier(couriers)
     # allows user to delete a courier from the list
     elif option == '4':
-        clear_screen()
-        if couriers != []:
-            couriers_list_index(couriers)
-            try:
-                courier_index = int(
-                    input('Enter index of courier you would like to remove\n> ').strip())
-                clear_screen()
-                courier_order = []
-                for i in range(len(orders)):
-                    if orders[i]["courier index"] == courier_index:
-                        courier_order.append(i)
-                    elif orders[i]["courier index"] > courier_index:
-                        orders[i]["courier index"] -= 1
-
-                while True:
-                    choice = input(f'''This courier is currently active in {len(courier_order)} orders
-
-Therefore please pick what you would like to do:
-
-Enter 1 to delete the courier and delete all orders associated with it
-
-Enter 2 to delete the courier and change the courier of all the associated couriers
-
-Enter 3 to not delete the courier
-> ''')
-
-                    if choice == '1':
-                        clear_screen()
-                        courier_name = couriers[courier_index]
-                        couriers.pop(courier_index)
-                        print(
-                            f'\n{courier_name} has been removed from your couriers\n')
-                        for i in courier_order[::-1]:
-                            orders.pop(i)
-                        break
-                    elif choice == '2':
-                        clear_screen()
-                        courier_name = couriers[courier_index]
-                        couriers.pop(courier_index)
-                        print(
-                            f'\n{courier_name} has been removed from your couriers\n')
-                        for i in courier_order:
-                            print(f'order to change: {orders[i]}')
-                            couriers_list_index(couriers)
-                            cour = int(
-                                input('\nWhich courier index would you like to switch to\n> '))
-                            orders[i]["courier index"] = cour
-                            clear_screen()
-                        break
-
-                    elif choice == '3':
-                        for i in range(len(orders)):
-                            if orders[i]["courier index"] >= courier_index:
-                                orders[i]["courier index"] += 1
-                        break
-
-            except:
-                print('\nYou have not entered a valid index\n')
-        else:
-            print('\nYou do not have any couriers to delete\n')
-        input('Press enter to continue')
+        couriers,orders = delete_courier(couriers,orders)
     # allows user to go back to previous menu
     elif option == '0':
         return False, couriers, orders
@@ -402,4 +404,4 @@ def order_menu_func(orders, status_list, couriers, order_menu_text):
 
 
 
-    
+
