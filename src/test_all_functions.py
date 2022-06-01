7b911a1e8fa49905e797a8fe59f8022d2630f35c
Behnam Vakili <behnamkvl@gmail.com>
2022-06-01 16:55:39 +0100

    checked multiple calls with call class

--- src/test_all_functions.py
+++ src/test_all_functions.py
@@ -1,7 +1,8 @@
+import unittest
 from all_functions import add_products,view_products
 from all_functions import Product
 
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 @patch('builtins.input',side_effect=['coke','1.7','25', ' '])
 def test_add_products(mock_input):
@@ -17,13 +18,12 @@ def test_add_products(mock_input):
     assert expected[0].__dict__ == actual[0].__dict__
 
 
-@patch('builtins.input',side_effect=[' '])
+@patch('builtins.input', side_effect=[' '])
 @patch('builtins.print')
-def test_view_products(mock_print,mock_input):
+def test_view_products(mock_print, mock_input):
     products = [Product(float('1.7'),25,'coke'),Product(float('1'),50,'bottle')]
-    ex_args = 'Product name: coke\nPrice: £1.7\nQuantity: 25\nIndex value: 0\n\nProduct name: bottle\nPrice: £1\nQuantity: 50\nIndex value: 1\n'
-    # ex_args2= 'Product name: bottle\nPrice: £1\nQuantity: 50\nIndex value: 1\n'
+    ex_args1 = 'Product name: coke\nPrice: £1.7\nQuantity: 25\nIndex value: 0\n'
+    ex_args2 = 'Product name: bottle\nPrice: £1.0\nQuantity: 50\nIndex value: 1\n'
     view_products(products)
 
-
-    mock_print.assert_called_with(ex_args)
+    mock_print.assert_has_calls([call(ex_args1), call(ex_args2)], any_order=True)
